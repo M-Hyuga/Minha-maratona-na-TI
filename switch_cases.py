0db0440d756c1693cfe6b24037692cc338cf6ed5
Mônica Nascimento <60589987+M-Hyuga@users.noreply.github.com>
2025-07-03 09:28:44 -0300

    Delete switch_cases.py

--- switch_cases.py
+++
@@ -1,5 +0,0 @@
-# O python não tem uma immplementação para os switch cases, mas uma maneira de implementá-los é com o dicionário, uma estrutura de dados que armazena o par de valores chaves.
-
-    # As condições switch são armazenadas como chaves no dicionário, e as ações armazenadas como valor.
-        # Se houver uma série de ações para cada caso, então considere escrever uma função para cada caso e use as chamadas de função como o valor.
-    
