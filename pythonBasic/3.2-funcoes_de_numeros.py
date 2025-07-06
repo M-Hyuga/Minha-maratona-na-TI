bd9eb5785760eeefd5510fcb794d7dde304e8469
Mônica Nascimento <60589987+M-Hyuga@users.noreply.github.com>
2025-07-06 03:13:50 -0300

    Update 3.2-funcoes_de_numeros.py

--- pythonBasic/3.2-funcoes_de_numeros.py
+++ pythonBasic/3.2-funcoes_de_numeros.py
@@ -1,2 +1,14 @@
-num1 = 1.234
-print(round(num1))  # Arredonda para o inteiro mais próximo
+#arredondando valores 
+num1 = 25.234
+print(round(num1, 1))  # Arredonda para o inteiro mais próximo, o numero após a virgula dita quantas casas decimais irão aparecer após a virgula.
+
+#facilitando exponenciação
+print(pow(2, 4))
+
+#max e min de uma lista 
+lista = 12,34,54,67,5,87,88,95
+print(max(lista))
+print(min(lista))
+
+#somar numeros dentro de uma lista 
+print(sum([1,43,56,78,33,21]))
