6f64e668c65e26f5240d9d8ff15f100d954c651f
Mônica Nascimento <60589987+M-Hyuga@users.noreply.github.com>
2025-07-09 04:18:17 -0300

    Create 3.3-modulo_math.py
    3-3-Falei sobre o modelo Math.

---
+++ pythonBasic/3.3-modulo_math.py
@@ -0,0 +1,16 @@
+#MÓDULO = conjunto de códigos que guardam funções para facilitar sua utilização quando chamadas. Pois depois de importado o  Math é só chamar a função sem a necessidade de escrever o código da mesma em si.
+#import é a função que chama todas as demais.
+import math
+
+
+#mostrar o teto de um número (arredonda pra cima)
+print(math.ceil(4))
+
+#mostrar o número arredondado pra baixo
+print(math.floor(4.1))
+
+#potencia
+print(math.pow(2,3)) 
+
+#raiz quadrada
+print(math.sqrt(9))
