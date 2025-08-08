74ed1bac0d5c40a9cad2aa24348a2d6c46eacd83
Mônica Nascimento <60589987+M-Hyuga@users.noreply.github.com>
2025-08-08 00:30:41 -0300

    métodos strings

---
+++ pythonBasic/4.1-metodos_em_strings.py
@@ -0,0 +1,21 @@
+#os métodos ajudam em situações como por exemplo padronizações, definir o que um objeto pode fazer, ou seja, seu comportamento... e assim vai
+mensagem =  "eu adoro comida caseira"
+print(mensagem.lower())
+
+print(mensagem.upper())
+
+print(mensagem.capitalize())
+
+print(mensagem.find('c'))
+
+print(mensagem.replace('comida caseira','pizza de calabreza'))
+
+print(mensagem.strip()) #retira os espaços na frente do conteúdo no inicio. (caso haja)
+
+'''lower
+upper
+capitalize 
+find 
+replace 
+strip 
+'''
---
+++ pythonBasic/7-if_else.py
@@ -0,0 +1,8 @@
+a = 12
+b = 4
+
+if a > b :
+    print("A é maior que B")
+
+else : 
+    print("A não é maior que B")
