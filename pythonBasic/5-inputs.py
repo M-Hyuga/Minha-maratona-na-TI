9eadbec48efcfd7128f1217ad88397f725fbbb52
Mônica Nascimento <60589987+M-Hyuga@users.noreply.github.com>
2025-12-03 01:27:35 -0300

    Update 5-inputs.py

--- pythonBasic/5-inputs.py
+++ pythonBasic/5-inputs.py
@@ -1,7 +1,2 @@
-nome = input("Qual é o seu nome? ")
-
-idade = input("Qual é sua idade? ")
-
-cidade = input("Qual a cidade que você mora? ")
-
-print( nome + " tem " + idade + " anos de idade e mora no " + cidade + ".")
+nome = input('Digite o seu nome :')
+print(nome)
